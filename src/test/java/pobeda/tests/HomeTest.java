eae373be0478173b2730ac485f590448eae46dbf
PGlitvinov <vtb4114175@corp.dev.vtb>
2025-11-12 13:42:54 +0300

    Задание №2. Page Object. Инициирование поиска

--- src/test/java/pobeda/tests/HomeTest.java
+++ src/test/java/pobeda/tests/HomeTest.java
@@ -66,4 +66,30 @@ public class HomeTest extends BaseTest {
                 .isEqualTo(DD_USEFUL);
 
     }
+
+    @Test
+    @DisplayName("Задание №2. Инициирование поиска")
+    void checkFlyingTicketSearch() {
+
+        homePage = new HomePage(driver, wait);
+        homePage.open();
+
+        homePage.moveToFormTicketSearch();
+
+        SoftAssertions softly = new SoftAssertions();
+        softly.assertThat(homePage.isVisible_inputToFormTicketSearch()).isTrue();
+        softly.assertThat(homePage.isVisible_inputFromFormTicketSearch()).isTrue();
+        softly.assertThat(homePage.isVisible_inputDepartureFormTicketSearch()).isTrue();
+        softly.assertThat(homePage.isVisible_inputReturnFormTicketSearch()).isTrue();
+        softly.assertAll();
+
+        homePage.setInputFromFormTicketSearch("Москва");
+        homePage.setInputToFormTicketSearch("Санкт-Петербург");
+
+        homePage.ticketSearch();
+
+        Assertions.assertThat(homePage.containerDepartureFormTicketSearchDataFailed())
+                .as("Поле \"Туда\" подсвечено красной рамкой")
+                .isTrue();
+    }
 }
